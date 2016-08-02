fb7868910337c77a8ed228099af2423d96068391
Doug McInnes <doug@dougmcinnes.com>
2016-08-01 21:34:33 -0700

    got a weather icon

--- MM-forecast-io.js
+++ MM-forecast-io.js
@@ -28,52 +28,43 @@ Module.register("MM-forecast-io", {
     return false;
   },
 
-  start: function() {
-    Log.info("Starting module: " + this.name);
-    this.scheduleUpdate(this.config.initialLoadDelay);
+  getScripts: function() {
+    return ['jsonp.js'];
   },
 
-  jsonp: function (url, callback) {
-    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
-    window[callbackName] = function(data) {
-      delete window[callbackName];
-      document.body.removeChild(script);
-      callback(data);
-    };
+  getStyles: function() {
+    return ["weather-icons.css", "MM-forecast-io.css"];
+  },
 
-    var script = document.createElement('script');
-    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
-    document.body.appendChild(script);
+  start: function() {
+    Log.info("Starting module: " + this.name);
+
+    if (localStorage.forecast) {
+      this.loaded = true
+      this.weatherData = JSON.parse(localStorage.forecast);
+      // this.updateDom(this.config.animationSpeed);
+      return;
+    }
+
+    this.scheduleUpdate(this.config.initialLoadDelay);
   },
 
   updateWeather: function() {
     var self = this;
     var retry = true;
     var url = 'https://api.forecast.io/forecast/'+this.config.apikey+'/'+this.config.latitude+','+this.config.longitude;
-    this.jsonp(url, this.processWeather.bind(this));
+    jsonp(url, this.processWeather.bind(this));
   },
 
   processWeather: function (data) {
     console.log('process', data);
+    this.loaded = true;
+    this.weatherData = data;
+    this.updateDom(this.config.animationSpeed);
   },
 
-  scheduleUpdate: function (delay) {
-    var nextLoad = this.config.updateInterval;
-    if (typeof delay !== "undefined" && delay >= 0) {
-      nextLoad = delay;
-    }
-
-    var self = this;
-    clearTimeout(this.updateTimer);
-    this.updateTimer = setTimeout(function() {
-      self.updateWeather();
-    }, nextLoad);
-  },
-
-  renderWeather: function (currentWeather, minuteWeather) {
+  renderWeather: function () {
     var temp = Math.round(currentWeather.temperature);
-
-    var wind = Math.round(currentWeather.windSpeed);
 
     var iconClass = iconTable[currentWeather.icon];
     var icon = $('<span/>').addClass('icon dimmed wi').addClass(iconClass);
@@ -99,29 +90,6 @@ Module.register("MM-forecast-io", {
     $('.weather-summary').updateWithText(summary, 1000);
   },
 
-  renderForecast: function (forcast) {
-    var forecastTable = $('<table />').addClass('forecast-table');
-    var weekday = (new Date()).getDay();
-    var days = forcast.data;
-    var opacity = 1;
-    for (var i = 0; i < days.length; i++) {
-      var day = days[i];
-      var row = $('<tr />').css('opacity', opacity);
-      if (i === 0) {
-        row.addClass('today');
-      }
-
-      row.append($('<td/>').addClass('day').html(moment.weekdaysShort(weekday)));
-      row.append($('<td/>').addClass('temp-min').html(Math.round(day.temperatureMin)));
-      row.append($('<td/>').addClass('temp-max').html(Math.round(day.temperatureMax)));
-      forecastTable.append(row);
-      weekday = (weekday + 1) % 7;
-      opacity -= 0.1;
-    }
-
-    $('.forecast').updateWithText(forecastTable, 1000);
-  },
-
   getDom: function() {
     var wrapper = document.createElement("div");
 
@@ -138,12 +106,27 @@ Module.register("MM-forecast-io", {
     }
 
     if (!this.loaded) {
+      wrapper.innerHTML = this.translate('LOADING');
+      wrapper.className = "dimmed light small";
       return wrapper;
     }
 
-    // var temp     = document.createElement("div");
+    var currentWeather = this.weatherData.currently;
+    var iconClass = this.config.iconTable[currentWeather.icon];
+    var icon = document.createElement("span");
+
+    var temp = document.createElement("div");
+    var tempValue = Math.round(currentWeather.temperature);
+    icon.className = 'wi weathericon ' + iconClass;
+    temp.appendChild(icon);
+    // temp.appendText(tempValue + '&deg;');
+
+    wrapper.appendChild(temp);
+
     // var summary  = document.createElement("div");
     // var forecast = document.createElement("div");
+
+    return wrapper;
   },
 
   scheduleUpdate: function(delay) {
