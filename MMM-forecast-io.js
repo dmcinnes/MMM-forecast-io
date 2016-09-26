5f02cd7a82169f253df6558fece9055dbe047498
Doug McInnes <doug@dougmcinnes.com>
2016-09-25 21:50:58 -0700

    no magic width numbers

--- MMM-forecast-io.js
+++ MMM-forecast-io.js
@@ -16,7 +16,7 @@ Module.register("MMM-forecast-io", {
     },
     latitude:  null,
     longitude: null,
-    width: 440,
+    forecastWidth: 410,
     testElementID: "forecast-io-test-element",
     unitTable: {
       'default':  'auto',
@@ -183,29 +183,29 @@ Module.register("MMM-forecast-io", {
   },
 
   renderForecastRow: function (data, min, max) {
-    var width = this.config.width - 20;
+    var width = this.config.forecastWidth;
     var row = document.createElement("div");
-    row.style.width = width;
     var total = max - min;
     var rowMin = Math.round(data.temperatureMin);
     var rowMax = Math.round(data.temperatureMax);
     var percentLeft  = Math.round(100 * ((rowMin - min) / total));
-    var percentRight = Math.round(100 * ((max - rowMax) / total));
     row.style["margin-left"]  = percentLeft + "%";
     row.className = "forecast-row";
     var minTempTextDiv = document.createElement("div");
     var minTempText = this.roundTemp(rowMin) + "\u00B0";
+    var minTempTextWidth = this.getTextWidth(minTempText);
     minTempTextDiv.innerHTML = minTempText;
-    minTempTextDiv.style.width = this.getTextWidth(minTempText) + "px";
+    minTempTextDiv.style.width = minTempTextWidth + "px";
     minTempTextDiv.className = "temp min-temp";
     var maxTempTextDiv = document.createElement("div");
     var maxTempText = this.roundTemp(rowMax) + "\u00B0";
+    var maxTempTextWidth = this.getTextWidth(maxTempText);
     maxTempTextDiv.innerHTML = maxTempText;
-    maxTempTextDiv.style.width = this.getTextWidth(maxTempText) + "px";
+    maxTempTextDiv.style.width = maxTempTextWidth + "px";
     maxTempTextDiv.className = "temp max-temp";
     var bar = document.createElement("div");
     bar.className = "bar";
-    var barWidth = width - 100;
+    var barWidth = width - minTempTextWidth - maxTempTextWidth;
     barWidth = Math.round(barWidth * ((rowMax - rowMin) / total));
     bar.style.width = barWidth + 'px';
     row.appendChild(minTempTextDiv);
@@ -214,27 +214,8 @@ Module.register("MMM-forecast-io", {
     return row;
   },
 
-  // Draw the weekly forecast as a graph, similar to how Dark Sky does.  Each day is drawn as a bar
-  // prepresenting the low and high temperature.
   renderWeatherForecast: function () {
-    // Add 10 pixels per extra decimal place so that the temperature labels fit
-    var marginL       = 85 + (this.config.tempDecimalPlaces * 10);
-    var marginR       = 17 + (this.config.tempDecimalPlaces * 10);
-    var marginT       =  2;
-    var marginB       = 10;
-    var lineMargin    =  8;
-    var dayRightEdge  = 37;
-    var barShift      =  8;
-    var barTextMargin =  2;
-    var barTextShift  = -2;
-    var iconRightEdge = dayRightEdge + 27;
-    var w = 200; // parseInt( $('.weekgraph').css('width') );
-    var h = 500; // parseInt( $('.weekgraph').css('height') );
-    var numDays       =  7;
-    var lineHeight    = (h - marginT - marginB) / numDays;
-    var opacityShift  =  0.12
-    var self          = this;
-
+    var numDays =  7;
     var i;
 
     var filteredDays =
@@ -258,153 +239,6 @@ Module.register("MMM-forecast-io", {
       display.appendChild(row);
     }
     return display;
-
-    // Set up the SVG
-    var weekGraphSVG = document.createElement("svg");
-    weekGraphSVG = d3.select(weekGraphSVG)
-      .attr('width',  w)
-      .attr('height', h)
-
-    // Set up the scale for the temps
-    var tempXScale = d3.scaleLinear().domain([ d3.min( filteredDays, function(d) { return d.temperatureMin } ),
-      d3.max( filteredDays, function(d) { return d.temperatureMax } ) ])
-        .range([ marginL, w - marginR ]);
-
-    // Add the freezing and hot lines
-    updateWeatherForcast_UpdateWeeklyGraph_HotColdLine( 32, "freezeLine", "\uf076",  0 )      // f076 is wi-snowflake-cold
-    updateWeatherForcast_UpdateWeeklyGraph_HotColdLine( 80, "hotLine",    "\uf072", -4 )      // f076 is wi-hot
-
-    // Draw labels down the left side
-    // - Create the labels
-    weekGraphSVG.selectAll( ".weekgraphDayText" )
-      .data( filteredDays )
-      .enter()
-      .append( "text" )
-      .attr(   "class", "weekgraphDayText" )
-      .attr(   "x", dayRightEdge )
-      .attr(   "text-anchor", "end")
-      .style(  "fill",    function(d,i) { return shadeColor2( "#DDDDDD", -i * opacityShift ) } );
-
-    // - Update all labels
-    weekGraphSVG.selectAll( ".weekgraphDayText" )
-      .text( function(d) {
-        var dt = new Date( d.time * 1000 );
-        return moment.weekdaysShort( dt.getDay() );
-      })
-      .attr( "y", function(d,i) {
-        return i * lineHeight + lineHeight + marginT;
-      });
-
-    // Draw the weather icon to the right of each day
-    // - Create the icon label
-    weekGraphSVG.selectAll( ".weekgraphDayIcon" )
-      .data( filteredDays )
-      .enter()
-      .append( "text" )
-      .attr(   "class", "weekgraphDayIcon wi" )
-      .attr(   "x", iconRightEdge )
-      .attr(   "text-anchor", "end")
-      .style(  "fill",    function(d,i) { return shadeColor2( "#DDDDDD", -i * opacityShift ) } );
-
-    var iconTable = this.config.svgIconTable;
-    weekGraphSVG.selectAll( ".weekgraphDayIcon" )
-      .text( function(d) {
-        return iconTable[ d.icon ];
-      })
-      .attr( "y", function(d,i) {
-        return i * lineHeight + lineHeight + marginT;
-      });
-
-    // Draw rounded rectangles for each day
-    // - Add one rect per day
-    weekGraphSVG.selectAll( ".weekgraphDayTempBar" )
-      .data( filteredDays )
-      .enter()
-      .append( "rect" )
-      .attr(   "class", "weekgraphDayTempBar" )
-      .attr(   "y",  function(d,i) {
-        return i * lineHeight + marginT + lineMargin + barShift;
-      })
-      .attr(   "height",   lineHeight - lineMargin*2  )
-      .attr(   "ry",      (lineHeight - lineMargin)/4 )
-      .attr(   "rx",      (lineHeight - lineMargin)/4 )
-      .style(  "fill",    function(d,i) { return shadeColor2( "#DDDDDD", -i * opacityShift ) } );
-
-    // - Update the left and right edges of the rect
-    weekGraphSVG.selectAll( ".weekgraphDayTempBar" )
-      .attr(   "x",     function(d,i) {
-        return tempXScale( d.temperatureMin );
-      })
-      .attr(   "width", function(d,i) {
-        return tempXScale( d.temperatureMax ) - tempXScale( d.temperatureMin );
-      });
-
-
-    // Draw temperature labels on the left and right side of each bar
-    // - Create min labels
-    weekGraphSVG.selectAll( ".weekgraphTempTextMin" )
-      .data( filteredDays )
-      .enter()
-      .append( "text" )
-      .attr(   "class", "weekgraphTempTextMin weekgraphTempText" )
-      .attr(   "text-anchor", "end")
-      .style(  "fill",    function(d,i) { return shadeColor2( "#DDDDDD", -i * opacityShift ) } );
-
-    // - Update all min labels
-    weekGraphSVG.selectAll( ".weekgraphTempTextMin" )
-      .text( function(d) {
-        return self.roundTemp( d.temperatureMin ).toString() + "\u00B0"; // Unicode for &deg;
-      })
-      .attr( "x", function(d,i) {
-        return tempXScale( d.temperatureMin ) - barTextMargin;
-      })
-      .attr( "y", function(d,i) {
-        return i * lineHeight + lineHeight + marginT + barTextShift;
-      });
-
-    // - Create max labels
-    weekGraphSVG.selectAll( ".weekgraphTempTextMax" )
-      .data( filteredDays )
-      .enter()
-      .append( "text" )
-      .attr(   "class", "weekgraphTempTextMax weekgraphTempText" )
-      .attr(   "text-anchor", "begin")
-      .style(  "fill",    function(d,i) { return shadeColor2( "#DDDDDD", -i * opacityShift ) } );
-
-    // - Update all max labels
-    weekGraphSVG.selectAll( ".weekgraphTempTextMax" )
-      .text( function(d) {
-        return self.roundTemp( d.temperatureMax ).toString() + "\u00B0"; // Unicode for &deg;
-      })
-      .attr( "x", function(d,i) {
-        return tempXScale( d.temperatureMax ) + barTextMargin;
-      })
-      .attr( "y", function(d,i) {
-        return i * lineHeight + lineHeight + marginT + barTextShift;
-      });
-
-    // from http://stackoverflow.com/questions/5560248/programmatically-lighten-or-darken-a-hex-color-or-rgb-and-blend-colors
-    function shadeColor2(color, percent) {
-      var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
-      return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
-    }
-
-    function updateWeatherForcast_UpdateWeeklyGraph_HotColdLine( temp, className, icon, offset ) {  // Subfunction of updateWeatherForcast_UpdateWeeklyGraph() for access to tempXScale
-      // Draw a line across the graph and place an icon at a given temperature
-      var tempPoint = tempXScale( temp );
-
-      // Add/update the line
-      if( weekGraphSVG.select( ".tempGraphHotColdLine ." + className ).empty() ) {
-        weekGraphSVG.append( "line").attr("class", "tempGraphHotColdLine " + className );
-      }
-
-      weekGraphSVG.select( ".tempGraphHotColdLine." + className).attr("x1", tempPoint ).attr("y1", marginT + lineMargin + barShift )
-        .attr("x2", tempPoint ).attr("y2", h - marginB )
-        .attr( "opacity", ((tempPoint > marginL) && (tempPoint < (w - marginR))) ? 1.0 : 0.0 );
-
-    }
-
-    return weekGraphSVG.node();
   },
 
   getLocation: function () {
